28d3d9ed3f563399f4aa90ebe1b53cec91567eae
Catgirl Rika <102428064+CatgirlRika@users.noreply.github.com>
2025-08-11 10:31:51 -0500

    Update main.tsx
    Signed-off-by: Catgirl Rika <102428064+CatgirlRika@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { Router as WouterRouter } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WouterRouter base={import.meta.env.BASE_URL}>
+      <App />
+    </WouterRouter>
+  </React.StrictMode>
+);
